3fe401f55c5fe7b1fff7025610523fc36c5652d3
Jim van Zuidam <jimvanzuidam@gmail.com>
2021-11-30 14:42:21 +0100

    Creating deck-list,new,edit,detail components

---
+++ src/app/components/deck/deck-new/deck-new.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeckNewComponent } from './deck-new.component';
+
+describe('DeckNewComponent', () => {
+  let component: DeckNewComponent;
+  let fixture: ComponentFixture<DeckNewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeckNewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DeckNewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
