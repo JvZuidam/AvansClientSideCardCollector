b43969779805b08e71f2891f5f4791eb380ff510
Jim van Zuidam <jimvanzuidam@gmail.com>
2022-04-09 18:28:38 +0200

    Trade new, update, detail, delete

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {userModel} from "../models/user.model";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {tap} from "rxjs";
+import {map, Observable, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -18,9 +18,9 @@ export class UserService {
     private http: HttpClient
   ) { }
 
-  getUsers() {
+  getUsers(): Observable<any> {
     console.log('getUsers aangeroepen');
-    return this.http.get<any>(environment.apiString + "/user", this.httpOptions).pipe(tap(result => console.log(result)))
+    return this.http.get<any>(environment.apiString + "/user", this.httpOptions).pipe(map(response => response.results));
   }
 
   getUserById() {
